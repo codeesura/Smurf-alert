9902a40b4d0690005ebbf361f03a2a18839198d8
codeesura <120671243+codeesura@users.noreply.github.com>
2023-01-05 17:15:42 +0300

    variable was added for the total supply

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+total_supply = 500 ## specific supply 
+
 @tasks.loop(seconds=1)
 async def status_task():
     try:
         getir = requests.get(f"https://api.polygonscan.com/api?module=stats&action=tokensupply&contractaddress=0xbac7e3182bb6691f180ef91f7ae4530abb3dc08d&apikey={config.POLYSCAN_TOKEN}").text
         getir = json.loads(getir)
     except : pass
-    if int(getir["result"]) > 425 :
+    if int(getir["result"]) > total_supply :
         channel = client.get_channel(int(config.CHANNEL_ID))
         await channel.send("@everyone")
         await channel.send("@everyone")
